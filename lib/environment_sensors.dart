a38b74bdcd00fb3608200f73b48b003c4cd07409
nhandrew <fourjulows@yahoo.com>
2021-03-09 10:45:18 -0500

    Dart fmt formatting

--- lib/environment_sensors.dart
+++ lib/environment_sensors.dart
@@ -1,24 +1,37 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+
 ///Main channel for method calls
-const MethodChannel _methodChannel = MethodChannel('environment_sensors/method');
+const MethodChannel _methodChannel =
+    MethodChannel('environment_sensors/method');
+
 ///Event channel for ambient temperature readings
-const EventChannel _temperatureEventChannel = EventChannel('environment_sensors/temperature');
+const EventChannel _temperatureEventChannel =
+    EventChannel('environment_sensors/temperature');
+
 ///Event channel for relative humdity readings
-const EventChannel _humidityEventChannel = EventChannel('environment_sensors/humidity');
+const EventChannel _humidityEventChannel =
+    EventChannel('environment_sensors/humidity');
+
 ///Event channel for ambient light readings
-const EventChannel _lightEventChannel = EventChannel('environment_sensors/light');
+const EventChannel _lightEventChannel =
+    EventChannel('environment_sensors/light');
+
 ///Event channel for pressure readings
-const EventChannel _pressureEventChannel = EventChannel('environment_sensors/pressure');
+const EventChannel _pressureEventChannel =
+    EventChannel('environment_sensors/pressure');
 
 class EnvironmentSensors {
   ///Stream of relative humidity readings
   Stream<double>? _humidityEvents;
+
   ///Stream of ambient temperature readings
   Stream<double>? _temperatureEvents;
+
   ///Stream of ambient light readings
   Stream<double>? _lightEvents;
+
   ///Stream of pressure readings
   Stream<double>? _pressureEvents;
 
@@ -38,38 +51,43 @@ class EnvironmentSensors {
 
   ///Gets the ambient temperature reading from device sensor, if present
   Stream<double> get temperature {
-    if (_temperatureEvents == null){
-      _temperatureEvents = _temperatureEventChannel.receiveBroadcastStream().map((event) => double.parse(event.toString()));
+    if (_temperatureEvents == null) {
+      _temperatureEvents = _temperatureEventChannel
+          .receiveBroadcastStream()
+          .map((event) => double.parse(event.toString()));
     }
     return _temperatureEvents!;
   }
 
   ///Gets the relative humidity reading from device sensor, if present
   Stream<double> get humidity {
-    if (_humidityEvents == null){
-      _humidityEvents = _humidityEventChannel.receiveBroadcastStream().map((event) => double.parse(event.toString()));
+    if (_humidityEvents == null) {
+      _humidityEvents = _humidityEventChannel
+          .receiveBroadcastStream()
+          .map((event) => double.parse(event.toString()));
     }
     return _humidityEvents!;
   }
 
   ///Gets the ambient light reading from device sensor, if present
   Stream<double> get light {
-    if (_lightEvents == null){
-      _lightEvents = _lightEventChannel.receiveBroadcastStream().map((event) => double.parse(event.toString()));
+    if (_lightEvents == null) {
+      _lightEvents = _lightEventChannel
+          .receiveBroadcastStream()
+          .map((event) => double.parse(event.toString()));
     }
     return _lightEvents!;
   }
 
   ///Gets the pressure reading from device sensor, if present
-  Stream<double> get pressure{
-    if (_pressureEvents == null){
-      _pressureEvents = _pressureEventChannel.receiveBroadcastStream().map((event) => double.parse(event.toString()));
+  Stream<double> get pressure {
+    if (_pressureEvents == null) {
+      _pressureEvents = _pressureEventChannel
+          .receiveBroadcastStream()
+          .map((event) => double.parse(event.toString()));
     }
     return _pressureEvents!;
   }
-
-
-
 }
 
 ///An enum for defining device types when checking for sensor availability
